a798a744bdd49ac8e7ba42297d99568131248cdc
Joao ugolini <joao_ugolini@estudante.sesisenai.org.br>
2024-07-02 16:10:48 -0300

    lista 2

---
+++ lista_02/exercicio_01/index.js
@@ -0,0 +1,10 @@
+
+let nome = prompt("Digite seu nome:");
+
+
+let email = prompt("Digite seu email:");
+
+
+console.log("Dados do usuário:");
+console.log("Nome: " + nome);
+console.log("Email: " + email);
